6bafdd755278add6d38a0fb3e0efcbb344e27084
LittleSweetEvil <40055618+LittleSweetLoli@users.noreply.github.com>
2024-03-17 19:17:30 +0500

    Segmented code only

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,22 @@ const inputElement = document.getElementById('csvFile');
 const showTableButton = document.getElementById('showTableButton');
 const showChartsButton = document.getElementById('showChartsButton');
 const showSurvivalChartsButton = document.getElementById('showSurvivalChartsButton');
+const tableContainer = document.getElementById('tableContainer');
+const chartContainer = document.getElementById('chartContainer');
 
 inputElement.addEventListener('change', handleFileSelect, false);
 showTableButton.addEventListener('click', toggleTable, false);
 showChartsButton.addEventListener('click', toggleCharts, false);
 showSurvivalChartsButton.addEventListener('click', showSurvivalCharts, false);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const inputElement = document.getElementById('csvFile');
+    const showTableButton = document.getElementById('showTableButton');
+    const showChartsButton = document.getElementById('showChartsButton');
+    const showSurvivalChartsButton = document.getElementById('showSurvivalChartsButton');
+
+    inputElement.addEventListener('change', handleFileSelect, false);
+    showTableButton.addEventListener('click', toggleTable, false);
+    showChartsButton.addEventListener('click', toggleCharts, false);
+    showSurvivalChartsButton.addEventListener('click', showSurvivalCharts, false);
+});
